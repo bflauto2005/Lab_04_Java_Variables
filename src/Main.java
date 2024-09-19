5c0d5c6ee0b1e71573ab0decbcbc96133d474197
bflauto2005 <bf201205@gmail.com>
2024-09-19 13:37:13 -0400

    Completed Part 1

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,44 @@ public class Main
 {
     public static void main(String[] args)
     {
+        int intOperandA = 30;
+        int intOperandB = 15;
+
+        int intSum = 0;
+        int intProduct = 0;
+        int intDifference = 0;
+        int intQuotient = 0;
+        int intModulo = 0;
+
+        double doubleOperandA = 24.24
+        double doubleOperandB = 3.15
+
+        double doubleSum = 0;
+        double doubleProduct = 0;
+        double doubleDifference = 0;
+        double doubleQuotient = 0;
+        double doubleModulo = 0;
+
+
+        intSum = intOperandA + intOperandB;
+        System.out.println("The sum using two ints of " + intOperandA + " and " + intOperandB + " is " + intSum);
+
+        intProduct = intOperandA * intOperandB;
+        System.out.println("The product using two ints of " + intOperandA + " and " + intOperandB + " is " + intProduct);
+
+        intDifference = intOperandA - intOperandB;
+        System.out.println("The difference using two ints of " + intOperandA + " and " + intOperandB + " is " + intDifference);
+
+        intQuotient = intOperandA / intOperandB;
+        System.out.println("The quotient using two ints of " + intOperandA + " and " + intOperandB + " is " + intQuotient);
+
+        intModulo = intOperandA % intOperandB;
+        System.out.println("The modulo using two ints of " + intOperandA + " and " + intOperandB + " is " + intModulo);
+
+
+        double lunchCost = 15.50;
+
+
 
     }
 }
